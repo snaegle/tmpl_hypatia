651a4fd9abf5a9baf1b53780ce0c1b8e848fa546
snaegle1 <naegle@sub.uni-goettingen.de>
2020-08-06 19:20:07 +0200

    Change nameing

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,23 +1,28 @@
 <?php
 
 /**
- * Extension Manager/Repository config file for ext "hypatia_science_consulting".
+ * Extension Manager/Repository config file for ext "tmpl_hypatia".
  */
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Hypatia Science Consulting',
-    'description' => '',
-    'category' => 'templates',
+    'description' => 'Template for Hypatia Science Consulting website',
+    'category' => 'plugin',
     'autoload' => [
         'psr-4' => [
-            'HypatiaScienceConsulting\\HypatiaScienceConsulting\\' => 'Classes',
+            'Snaegle\\TmplHypatia\\' => 'Classes',
         ],
     ],
-    'state' => 'stable',
+    'state' => 'alpha',
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
     'author' => 'Sibylle Nägle',
     'author_email' => 'admin@science-consulting.info',
     'author_company' => 'Hypatia Science Consulting',
-    'version' => '1.0.0',
+    'version' => '0.0.2',
+    'constraints' => [
+      'depends' => [
+        'typo3' => '9.5-10.9.99',
+      ],
+    ],
 ];
--- ext_localconf.php
+++ ext_localconf.php
@@ -4,9 +4,9 @@ defined('TYPO3_MODE') || die();
 /***************
  * Add default RTE configuration
  */
-$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['hypatia_science_consulting'] = 'EXT:hypatia_science_consulting/Configuration/RTE/Default.yaml';
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['tmpl_hypatia'] = 'EXT:tmpl_hypatia/Configuration/RTE/Default.yaml';
 
 /***************
  * PageTS
  */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:hypatia_science_consulting/Configuration/TsConfig/Page/All.tsconfig">');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tmpl_hypatia/Configuration/TsConfig/Page/All.tsconfig">');
